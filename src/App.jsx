11b18fd33730f804f020b8326c9efec37111e315
Enock <enock.mokua@student.moringaschool.com>
2023-04-19 18:56:29 +0300

    create layout structure for section2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { useState } from "react";
 import "./App.css";
 import Landing from "./pages/landing/landing";
 import Footer from "./components/footer/footer";
--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -11,12 +11,24 @@ export default function Landing() {
 
       <section id="landing-page-2">
         <div id="landing-page-2-head">
-          <h3>Browse all categories</h3>
+          <h2>Browse all categories</h2>
           <p>Find what you want and what you need.</p>
         </div>
-        <div id="landing-page-2-body"></div>
-        <div id="landing-page-2-foot"></div>
+
+        <div id="landing-page-2-body">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div id="landing-page-2-foot">
+          <span></span>
+          <span></span>
+        </div>
       </section>
+
       <section id="landing-page-3"></section>
       <section id="landing-page-4"></section>
       <section id="landing-page-5"></section>
