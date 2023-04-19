3a01115f3c9b264f6e8477dfd12f121e05270a23
Enock <enock.mokua@student.moringaschool.com>
2023-04-19 12:24:42 +0300

    create the landing page component

---
+++ src/pages/landing/landing.jsx
@@ -0,0 +1,6 @@
+import "./landing.css";
+import React, { useState, useEffect } from "react";
+
+export default function Landing() {
+  return <main id="landing-page">This is the landing page</main>;
+}
